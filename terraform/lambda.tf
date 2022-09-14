f9f1045b495c00ff495f7e609d2137a062c9b311
Gary E. Walker <gewalker@gmail.com>
2022-09-14 15:04:20 -0400

    Fmt all the things

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -52,30 +52,30 @@ resource "aws_api_gateway_method" "proxy" {
 }
 
 resource "aws_api_gateway_integration" "this" {
-  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
 
   integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = "${aws_lambda_function.this.invoke_arn}"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.this.invoke_arn
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-  resource_id = "${aws_api_gateway_rest_api.this.root_resource_id}"
-  http_method = "ANY"
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_rest_api.this.root_resource_id
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = "${aws_lambda_function.this.invoke_arn}"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.this.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "this" {
@@ -84,15 +84,15 @@ resource "aws_api_gateway_deployment" "this" {
     "aws_api_gateway_integration.lambda_root",
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-  stage_name = "dev"
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name  = "dev"
 }
 
 resource "aws_lambda_permission" "api_gateway_invoke" {
-  statement_id = "AllowAPIGatewayInvoke"
-  action = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.this.function_name}"
-  principal = "apigateway.amazonaws.com"
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.this.execution_arn}/*/*"
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,18 +5,18 @@
 
 variable "s3_bucket_name" {
   type        = string
-  default = "lambdas.gewalker.net"
+  default     = "lambdas.gewalker.net"
   description = "S3 bucket in which the code package for this lambda function is stored."
 }
 
 variable "s3_key" {
   type        = string
-  default = "tls_validator/function.zip"
+  default     = "tls_validator/function.zip"
   description = "The AWS S3 key/path to the function code package."
 }
 
 variable "lambda_name" {
   type        = string
-  default = "tls_validator"
+  default     = "tls_validator"
   description = "The name for the lambda function ()"
 }
